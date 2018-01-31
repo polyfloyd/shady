c5232835ceaa4f42dd5a876a81dcf53c23c72e9d
polyfloyd <floyd@polyfloyd.net>
2018-01-31 19:44:54 +0100

    Really fix environment detection

---
+++ cmd/shady/environment_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDetectEnvironmentShaderToy(t *testing.T) {
+	sources := []string{
+		`void mainImage( out vec4 fragColor, in vec2 fragCoord ) { }`,
+		`void mainImage(out vec4 foo,in vec2 bar){}`,
+		`void   mainImage  (  out  vec4  o  ,  in   vec2  i  )  {  }`,
+	}
+	for _, s := range sources {
+		env, ok := DetectEnvironment(s)
+		if !ok {
+			t.Fatalf("unable to detect environment from source: %q", s)
+		}
+		if _, ok := env.(ShaderToy); !ok {
+			t.Fatalf("detect environment is not ShaderToy for source: %q", s)
+		}
+	}
+}
+
+func TestDetectEnvironmentGLSLSandbox(t *testing.T) {
+	sources := []string{
+		`uniform vec2 resolution;`,
+		`uniform  vec2  resolution;`,
+	}
+	for _, s := range sources {
+		env, ok := DetectEnvironment(s)
+		if !ok {
+			t.Fatalf("unable to detect environment from source: %q", s)
+		}
+		if _, ok := env.(GLSLSandbox); !ok {
+			t.Fatalf("detect environment is not ShaderToy for source: %q", s)
+		}
+	}
+}
