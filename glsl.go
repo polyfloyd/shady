cc2a2285affcc12b1e255da7da49eb949f9c7f26
polyfloyd <floyd@polyfloyd.net>
2018-02-10 00:39:12 +0100

    Separate rendering logic from shader management
    This should make it easier to understand, improve and replace the
    current implementation.

--- glsl.go
+++ glsl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"io"
 	"os"
 	"strings"
 	"sync"
@@ -16,12 +17,6 @@ import (
 
 var glfwInitOnce sync.Once
 
-type target struct {
-	texture      uint32
-	renderbuffer uint32
-	framebuffer  uint32
-}
-
 type Shader struct {
 	w, h uint
 
@@ -29,11 +24,9 @@ type Shader struct {
 	vertLoc uint32
 	canvas  uint32
 
-	targets        [2]target
-	curTargetIndex int
-
 	uniforms map[string]Uniform
 	env      Environment
+	renderer renderer
 	program  uint32
 }
 
@@ -46,21 +39,20 @@ func NewShader(width, height uint, env Environment) (*Shader, error) {
 		return nil, err
 	}
 
-	glfw.WindowHint(glfw.Visible, 0)
+	glfw.WindowHint(glfw.Visible, glfw.False)
 	glfw.WindowHint(glfw.RedBits, 8)
 	glfw.WindowHint(glfw.GreenBits, 8)
 	glfw.WindowHint(glfw.BlueBits, 8)
 	glfw.WindowHint(glfw.AlphaBits, 8)
-	glfw.WindowHint(glfw.DoubleBuffer, 0)
+	glfw.WindowHint(glfw.DoubleBuffer, glfw.False)
 	win, err := glfw.CreateWindow(1<<12, 1<<12, "glsl", nil, nil)
 	if err != nil {
 		return nil, err
 	}
 	sh := &Shader{
-		win: win,
-		w:   width,
-		h:   height,
-		env: env,
+		win:      win,
+		env:      env,
+		renderer: &textureRenderer{w: width, h: height},
 	}
 	sh.win.MakeContextCurrent()
 
@@ -79,26 +71,8 @@ func NewShader(width, height uint, env Environment) (*Shader, error) {
 	}()
 
 	// Set up the render targets.
-	for i := range sh.targets {
-		t := &sh.targets[i]
-
-		gl.GenTextures(1, &t.texture)
-		gl.BindTexture(gl.TEXTURE_2D, t.texture)
-		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(width), int32(height), 0, gl.RGBA, gl.UNSIGNED_BYTE, nil)
-		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-
-		gl.GenFramebuffers(1, &t.framebuffer)
-		gl.BindFramebuffer(gl.FRAMEBUFFER, t.framebuffer)
-		gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, t.texture, 0)
-		gl.GenRenderbuffers(1, &t.renderbuffer)
-		gl.BindRenderbuffer(gl.RENDERBUFFER, t.renderbuffer)
-
-		gl.BindTexture(gl.TEXTURE_2D, 0)
-		gl.BindRenderbuffer(gl.RENDERBUFFER, 0)
-		gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
+	if err := sh.renderer.Setup(); err != nil {
+		return nil, err
 	}
 
 	// Create the canvas.
@@ -139,53 +113,48 @@ func NewShader(width, height uint, env Environment) (*Shader, error) {
 	return sh, nil
 }
 
-func (sh *Shader) downloadImage(targetIndex int) image.Image {
-	img := image.NewRGBA(image.Rect(0, 0, int(sh.w), int(sh.h)))
-	gl.BindTexture(gl.TEXTURE_2D, sh.targets[targetIndex].texture)
-	gl.GetTexImage(gl.TEXTURE_2D, 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(&img.Pix[0]))
-	gl.BindTexture(gl.TEXTURE_2D, 0)
-	return img
-}
-
-func (sh *Shader) drawImage(targetIndex int) {
-	gl.BindFramebuffer(gl.FRAMEBUFFER, sh.targets[targetIndex].framebuffer)
-	gl.Clear(gl.COLOR_BUFFER_BIT)
-	gl.DrawArrays(gl.TRIANGLE_STRIP, 0, 4)
-	gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
-}
-
 func (sh *Shader) Image() image.Image {
 	sh.env.PreRender(sh.uniforms, RenderState{
 		Time:         0,
 		CanvasWidth:  sh.w,
 		CanvasHeight: sh.h,
 	})
-	sh.drawImage(0)
-	return sh.downloadImage(0)
+	handle := sh.renderer.Draw()
+	return sh.renderer.Image(handle)
 }
 
 func (sh *Shader) Animate(ctx context.Context, interval time.Duration, stream chan<- image.Image) {
 	var t time.Duration
+	var prevImageHandle interface{}
+	buffer := make(chan interface{}, sh.renderer.NumBuffers())
 	for frame := uint64(0); ; frame++ {
-		curTarget := int(frame & 1)
-		prevTarget := int(frame&1) ^ 1
+		var prevTexID uint32
+		if prevImageHandle != nil {
+			prevTexID = sh.renderer.Texture(prevImageHandle)
+		}
 		sh.env.PreRender(sh.uniforms, RenderState{
 			Time:               t,
 			Interval:           interval,
 			FramesProcessed:    frame,
 			CanvasWidth:        sh.w,
 			CanvasHeight:       sh.h,
-			PreviousFrameTexID: sh.targets[prevTarget].texture,
+			PreviousFrameTexID: prevTexID,
 		})
 		t += interval
 
-		sh.drawImage(curTarget)
-		if frame == 0 {
-			// Give the first render time to complete.
+		handle := sh.renderer.Draw()
+		buffer <- handle
+		prevImageHandle = handle
+
+		if prevImageHandle != nil {
+			sh.renderer.FreeTexture(prevTexID)
+		}
+		if len(buffer) != cap(buffer) {
+			// Give the first renders time to complete.
 			continue
 		}
 
-		img := sh.downloadImage(prevTarget)
+		img := sh.renderer.Image(<-buffer)
 		select {
 		case <-ctx.Done():
 			return
@@ -197,12 +166,10 @@ func (sh *Shader) Animate(ctx context.Context, interval time.Duration, stream ch
 func (sh *Shader) Close() error {
 	gl.DeleteProgram(sh.program)
 	gl.DeleteBuffers(1, &sh.canvas)
-	for _, t := range sh.targets {
-		gl.DeleteBuffers(1, &t.framebuffer)
-		gl.DeleteBuffers(1, &t.renderbuffer)
-		gl.DeleteTextures(1, &t.texture)
+	defer sh.win.Destroy()
+	if err := sh.renderer.Close(); err != nil {
+		return err
 	}
-	sh.win.Destroy()
 	return nil
 }
 
@@ -214,4 +181,89 @@ type Flip struct {
 func (flip *Flip) At(x, y int) color.Color {
 	h := flip.Bounds().Dy()
 	return flip.Image.At(x, h-y-1)
+}
+
+type renderer interface {
+	io.Closer
+	Setup() error
+	NumBuffers() int
+
+	Draw() (handle interface{})
+	Image(handle interface{}) image.Image
+
+	Texture(handle interface{}) uint32
+	FreeTexture(id uint32)
+}
+
+type textureRenderer struct {
+	w, h           uint
+	curTargetIndex int
+	targets        [2]struct {
+		texture      uint32
+		renderbuffer uint32
+		framebuffer  uint32
+	}
+}
+
+func (tr *textureRenderer) Setup() error {
+	for i := range tr.targets {
+		t := &tr.targets[i]
+
+		gl.GenTextures(1, &t.texture)
+		gl.BindTexture(gl.TEXTURE_2D, t.texture)
+		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(tr.w), int32(tr.h), 0, gl.RGBA, gl.UNSIGNED_BYTE, nil)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+
+		gl.GenFramebuffers(1, &t.framebuffer)
+		gl.BindFramebuffer(gl.FRAMEBUFFER, t.framebuffer)
+		gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, t.texture, 0)
+		gl.GenRenderbuffers(1, &t.renderbuffer)
+		gl.BindRenderbuffer(gl.RENDERBUFFER, t.renderbuffer)
+
+		gl.BindTexture(gl.TEXTURE_2D, 0)
+		gl.BindRenderbuffer(gl.RENDERBUFFER, 0)
+		gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
+	}
+	return nil
+}
+
+func (tr *textureRenderer) NumBuffers() int {
+	return len(tr.targets)
+}
+
+func (tr *textureRenderer) Image(handle interface{}) image.Image {
+	i := handle.(int)
+	img := image.NewRGBA(image.Rect(0, 0, int(tr.w), int(tr.h)))
+	gl.BindTexture(gl.TEXTURE_2D, tr.targets[i].texture)
+	gl.GetTexImage(gl.TEXTURE_2D, 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(&img.Pix[0]))
+	gl.BindTexture(gl.TEXTURE_2D, 0)
+	return img
+}
+
+func (tr *textureRenderer) Draw() interface{} {
+	tr.curTargetIndex = (tr.curTargetIndex + 1) % len(tr.targets)
+	i := tr.curTargetIndex
+	gl.BindFramebuffer(gl.FRAMEBUFFER, tr.targets[i].framebuffer)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+	gl.DrawArrays(gl.TRIANGLE_STRIP, 0, 4)
+	gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
+	return i
+}
+
+func (tr *textureRenderer) Texture(handle interface{}) uint32 {
+	return tr.targets[handle.(int)].texture
+}
+
+func (tr *textureRenderer) FreeTexture(id uint32) {}
+
+func (tr *textureRenderer) Close() error {
+	for _, t := range tr.targets {
+		gl.DeleteBuffers(1, &t.framebuffer)
+		gl.DeleteBuffers(1, &t.renderbuffer)
+		gl.DeleteTextures(1, &t.texture)
+	}
+	return nil
 }
