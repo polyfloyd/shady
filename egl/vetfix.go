41d0d11082b6d8c0d6bae996958c2de1cd6615d7
polyfloyd <floyd@polyfloyd.net>
2018-06-09 14:37:33 +0200

    Typo

--- egl/vetfix.go
+++ egl/vetfix.go
@@ -1,5 +1,5 @@
 package egl
 
-// 2017-03-08 go vet thinks that cgo files are not Go files and thus fails.
+// 2018-03-08 go vet thinks that cgo files are not Go files and thus fails.
 // This file is here to trick vet into thinking that the egl package contains
 // Go source files.
