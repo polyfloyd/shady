d41f58619e3e8150096cd71eb3e47dad786556ae
polyfloyd <floyd@polyfloyd.net>
2018-12-20 18:11:40 +0100

    Implement some basic error log parsing

--- renderer/compile.go
+++ renderer/compile.go
@@ -1,13 +1,15 @@
 package renderer
 
 import (
+	"bytes"
+	"fmt"
 	"io"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
 )
-
-const sourceSeparator = "\n\n"
 
 func compileShader(stage Stage, sources ...Source) (uint32, error) {
 	glStage, err := stage.glEnum()
@@ -15,14 +17,19 @@ func compileShader(stage Stage, sources ...Source) (uint32, error) {
 		return 0, err
 	}
 
-	var src string
-	for _, s := range sources {
+	originalSources := make([]string, len(sources))
+	src := ""
+	for i, s := range sources {
 		c, err := s.Contents()
 		if err != nil {
 			return 0, err
 		}
+		originalSources[i] = string(c)
+		if i != 0 {
+			src += fmt.Sprintf("#line 1 %d\n", i)
+		}
 		src += string(c)
-		src += sourceSeparator
+		src += "\n\n"
 	}
 
 	shader := gl.CreateShader(glStage)
@@ -40,7 +47,7 @@ func compileShader(stage Stage, sources ...Source) (uint32, error) {
 		gl.GetShaderInfoLog(shader, logLen, nil, gl.Str(log))
 		gl.DeleteShader(shader)
 		return 0, CompileError{
-			sources: sources,
+			sources: originalSources,
 			stage:   stage,
 			log:     log,
 		}
@@ -98,23 +105,59 @@ func linkProgram(sources map[Stage][]Source) (uint32, error) {
 }
 
 type CompileError struct {
-	sources []Source
+	sources []string
 
 	stage Stage
 	log   string
 }
 
-func (err CompileError) Error() (str string) {
+func (err CompileError) Error() string {
+	var buf bytes.Buffer
 	if err.stage == StageVertex {
-		str += "Error compiling vertex shader:\n"
+		fmt.Fprintf(&buf, "Error compiling vertex shader:\n")
 	} else if err.stage == StageFragment {
-		str += "Error compiling fragment shader:\n"
+		fmt.Fprintf(&buf, "Error compiling fragment shader:\n")
 	}
-	str += err.log
-	return
+	err.PrettyPrint(&buf)
+	return buf.String()
 }
 
-func (err CompileError) PrettyPrint(out io.Writer, color bool) {
+func (err CompileError) PrettyPrint(out io.Writer) {
+	markers := err.markers()
+	if len(markers) == 0 {
+		fmt.Fprintf(out, "%s\n", err.log)
+	}
+
+	for _, marker := range markers {
+		lines := strings.Split(err.sources[marker.fileno], "\n")
+		for i := marker.lineno - 2; i < marker.lineno+2; i++ {
+			if 0 <= i && i < len(lines) {
+				fmt.Fprintf(out, "%04d: %s\n", i+1, lines[i])
+			}
+			if i+1 == marker.lineno {
+				fmt.Fprintf(out, "      ^ %s\n", marker.message)
+			}
+		}
+	}
+}
+
+func (err CompileError) markers() []errorMarker {
+	errLineRe := regexp.MustCompile(`(?m)^(\d+):(\d+)\((\d+)\): (.+)$`)
+
+	var markers []errorMarker
+	matches := errLineRe.FindAllStringSubmatch(err.log, -1)
+	for _, m := range matches {
+		fileno, _ := strconv.Atoi(m[1])
+		lineno, _ := strconv.Atoi(m[2])
+		message := m[4]
+
+		markers = append(markers, errorMarker{
+			fileno:  fileno,
+			lineno:  lineno,
+			message: message,
+		})
+	}
+	return markers
 }
 
 type LinkError struct {
@@ -125,4 +168,10 @@ type LinkError struct {
 
 func (err LinkError) Error() (str string) {
 	return err.log
+}
+
+type errorMarker struct {
+	lineno  int
+	fileno  int
+	message string
 }
--- renderer/compile_test.go
+++ renderer/compile_test.go
@@ -4,20 +4,57 @@ import (
 	"testing"
 )
 
-func TestUnknownVar(t *testing.T) {
+func TestSingleFileMarkerPosition(t *testing.T) {
 	initTestGL(t)
 
-	sources := SourceBuf(`
+	source := SourceBuf(`
 void main() {
-	a = 12;
+	#error meh
 }
 	`)
 
-	_, err := compileShader(StageVertex, sources)
-	compileError, ok := err.(CompileError)
-	if !ok {
-		t.Fatalf("expected a CompileError, got %#v", err)
-	}
+	_, err := compileShader(StageVertex, source)
+	cerr := err.(CompileError)
 
-	_ = compileError
+	t.Logf("\n%s\n", cerr.log)
+
+	m := cerr.markers()
+	if len(m) == 0 {
+		t.Fatalf("Expected at least one error marker")
+	}
+	if m[0].fileno != 0 {
+		t.Fatalf("Unexpected fileno")
+	}
+	if m[0].lineno != 3 {
+		t.Fatalf("Unexpected lineno")
+	}
+}
+
+func TestMultiFileMarkerPosition(t *testing.T) {
+	initTestGL(t)
+
+	source1 := SourceBuf(`
+// A bunch of text to offset the line number.
+	`)
+	source2 := SourceBuf(`
+void main() {
+	#error meh
+}
+	`)
+
+	_, err := compileShader(StageVertex, source1, source2)
+	cerr := err.(CompileError)
+
+	t.Logf("\n%s\n", cerr.log)
+
+	m := cerr.markers()
+	if len(m) == 0 {
+		t.Fatalf("Expected at least one error marker")
+	}
+	if m[0].fileno != 1 {
+		t.Fatalf("Unexpected fileno")
+	}
+	if m[0].lineno != 3 {
+		t.Fatalf("Unexpected lineno")
+	}
 }
